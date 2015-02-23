ddd5adc99ba0852e6eeaeb0ed205bbe0342f49c8
Joseph Le <josephle@me.com>
2015-02-23 12:48:26 -0800

    Added mentor model

---
+++ models/Mentor.js
@@ -0,0 +1,32 @@
+Mentor = new Mongo.Collection('Mentor');
+
+Mentor.attachSchema(
+    new SimpleSchema({
+    name: {
+      type: String
+    },
+    subject: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      denyUpdate: true
+    }
+  })
+);
+
+// Collection2 already does schema checking
+// Add custom permission rules if needed
+if (Meteor.isServer) {
+  Mentor.allow({
+    insert : function () {
+      return true;
+    },
+    update : function () {
+      return true;
+    },
+    remove : function () {
+      return true;
+    }
+  });
+}
