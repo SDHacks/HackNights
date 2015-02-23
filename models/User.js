d045511097d11171b9cb23893920bb7f262af366
Joseph Le <josephle@me.com>
2015-02-23 15:36:23 -0800

    Added user profile schema to extend default

---
+++ models/User.js
@@ -0,0 +1,73 @@
+userSchema = {};
+
+userSchema.UserProfile = new SimpleSchema({
+  firstName: {
+    type: String,
+    regEx: /^[a-zA-Z-]{2,25}$/
+  },
+  lastName: {
+    type: String,
+    regEx: /^[a-zA-Z]{2,25}$/
+  },
+  birthday: {
+    type: Date,
+    optional: true
+  },
+  gender: {
+    type: String,
+    optional: true
+  },
+  website: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Url,
+    optional: true
+  },
+  major: {
+    type: String,
+    optional: true
+  },
+  classLevel: {
+    type: String,
+    allowedValues: ['First', 'Second', 'Third', 'Fourth', 'Other'],
+    optional: true
+  }
+});
+
+userSchema.User = new SimpleSchema({
+  username: {
+    type: String,
+    regEx: /^[a-z0-9A-Z_]{3,15}$/
+  },
+  emails: {
+    type: [Object],
+    // this must be optional if you also use other login services like facebook,
+    // but if you use only accounts-password, then it can be required
+    optional: true
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  "emails.$.verified": {
+    type: Boolean
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: userSchema.UserProfile,
+    optional: true
+  },
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  roles: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  }
+});
+
+Meteor.users.attachSchema(userSchema.User);
