44bf3d8e01b044e96a55baa7821d2939b963ea54
Joseph Le <josephle@me.com>
2015-02-23 14:43:01 -0800

    Allowed hackers to be added to queue

--- client/views/helpQueue/helpQueue.js
+++ client/views/helpQueue/helpQueue.js
@@ -2,13 +2,27 @@ Meteor.subscribe('hackers');
 Meteor.subscribe('mentors');
 
 Template['helpQueue'].helpers({
-  'hackers' : function() {
+  hackers: function() {
     return Hackers.find();
   },
-  'mentors' : function() {
+  mentors: function() {
     return Mentors.find();
   }
 });
 
 Template['helpQueue'].events({
+  'submit #addHackerForm': function(e) {
+    e.preventDefault(); // Prevent form submission from reloading page
+
+    var fields = {
+      name: e.target.name.value,
+      subject: e.target.subject.value,
+      location: e.target.location.value
+    };
+
+    // Insert hacker into queue
+    Hackers.insert(fields, function() {
+      Flash.success('<strong>Your request for help has been received!</strong>');
+    });
+  }
 });
