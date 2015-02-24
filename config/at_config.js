d016bfaaa6c40cf652bd6bebb52ea79661ee99ad
Joseph Le <josephle@me.com>
2015-02-23 16:31:29 -0800

    Added first and last name to registration

---
+++ config/at_config.js
@@ -0,0 +1,13 @@
+AccountsTemplates.addField({
+  _id: 'firstName',
+  type: 'text',
+  displayName: "First Name",
+  placeholder: "First Name"
+});
+
+AccountsTemplates.addField({
+  _id: 'lastName',
+  type: 'text',
+  displayName: "Last Name",
+  placeholder: "Last Name"
+});
